c532b293ba9ac05e0125fe3acad63ba8b4262e5a
Rachel Quilligan <rachel.quilligan@globalx.com.au>
2023-04-11 13:28:18 -0400

    initial commit

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            {
+                new ApiScope("jewels", displayName: "JewelsAPI")
+             };
+
+    public static IEnumerable<Client> Clients =>
+    new List<Client>
+    {
+        new Client
+        {
+            ClientId = "client",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("secret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "jewels" }
+        }
+    };
+}
